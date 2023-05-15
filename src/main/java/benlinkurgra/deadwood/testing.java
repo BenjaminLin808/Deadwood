b79da09a244b2deeaaf5e245d81bdcfb7d19301a
Kurt <kgraef@live.com>
2023-05-14 21:24:30 -0700

    fix shadow jar build

--- src/main/java/benlinkurgra/deadwood/testing.java
+++ src/main/java/benlinkurgra/deadwood/testing.java
@@ -1,12 +1,12 @@
-package benlinkurgra.deadwood;
-
-import benlinkurgra.deadwood.model.Player;
-
-public class testing {
-    public static void main(String[] args) {
-        int rank = 2;
-        int credits = 4;
-        Player player = new Player("testingName", credits, rank);
-        System.out.println(player.getActingRank());
-    }
-}
+//package benlinkurgra.deadwood;
+//
+//import benlinkurgra.deadwood.model.Player;
+//
+//public class testing {
+//    public static void main(String[] args) {
+//        int rank = 2;
+//        int credits = 4;
+//        Player player = new Player("testingName", credits, rank);
+//        System.out.println(player.getActingRank());
+//    }
+//}
